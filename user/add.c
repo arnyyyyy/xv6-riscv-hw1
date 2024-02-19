8d790ddfa9ac4241b643a7d2bc77bc8ecd6f3396
arnyyyyy <garrybobcrabsburger@gmail.com>
2024-02-19 19:12:00 +0300

    only non-negative numbers

--- user/add.c
+++ user/add.c
@@ -2,7 +2,6 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-
 int count_spaces(char *str) {
     int count = 0;
     while (*str) {
@@ -23,50 +22,6 @@ int count_digits(char *str) {
         str++;
     }
     return count;
-}
-
-int count_signs(char *str) {
-    int count = 0;
-    while (*str) {
-        if (*str == '-') {
-            count++;
-        }
-        str++;
-    }
-    return count;
-}
-
-int signed_atoi(char *str) {
-    int is_neg = 0;
-    if (str[0] == '-') {
-        is_neg = 1;
-        str++;
-    }
-
-    int num = atoi(str);
-    if (is_neg) {
-        num *= -1;
-    }
-
-    return num;
-}
-
-int check_number(const char *str) {
-    int seen_digit_or_space = 0;
-
-    while (*str != '\0' && *str != ' ') {
-        if (*str >= '0' && *str <= '9') {
-            seen_digit_or_space = 1;
-        }
-        if (*str == '-') {
-            if (seen_digit_or_space) {
-                return 0;
-            }
-            seen_digit_or_space = 1;
-        }
-        str++;
-    }
-    return 1;
 }
 
 int count_starting_spaces(char *str) {
@@ -95,7 +50,7 @@ int count_ending_spaces(char *str) {
     return cnt;
 }
 
-int count_numbers_(char *str) {
+int count_numbers(char *str) {
     int seen_digit = 0;
     int cnt = 0;
     while (*str != '\0') {
@@ -111,10 +66,6 @@ int count_numbers_(char *str) {
             str++;
             seen_digit = 1;
         }
-
-        if (*str == '-') {
-            str++;
-        }
     }
 
     if (seen_digit) {
@@ -122,6 +73,7 @@ int count_numbers_(char *str) {
     }
     return cnt;
 }
+
 
 int main(int argc, char *argv[]) {
     int BUF_SIZE = 50;
@@ -138,17 +90,11 @@ int main(int argc, char *argv[]) {
 
     int spaces = count_spaces(buf);
     int digits = count_digits(buf);
-    int signs = count_signs(buf);
     int extra_starting_spaces = count_starting_spaces(buf);
     int extra_ending_spaces = count_ending_spaces(buf + extra_starting_spaces);
 
-    if (spaces + digits + signs != BUF_SIZE - 1) {
+    if (spaces + digits != BUF_SIZE - 1) {
         printf("Invalid args: unexpected symbol");
-        exit(1);
-    }
-
-    if (signs >= 3) {
-        printf("Invalid args: extra symbol '-'");
         exit(1);
     }
 
@@ -162,9 +108,9 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    int count_numbers = count_numbers_(buf);
+    int numbers_counter = count_numbers(buf);
 
-    if (count_numbers != 2) {
+    if (numbers_counter != 2) {
         printf("Invalid number of args: two numbers expected");
         exit(1);
     }
@@ -176,17 +122,8 @@ int main(int argc, char *argv[]) {
 
     strcpy(first_num_s, buf + extra_starting_spaces);
     strcpy(second_num_s, buf + strlen(first_num_s) + extra_starting_spaces + 1);
-    if (!check_number(first_num_s)) {
-        printf("Incorrect number");
-        exit(1);
-    }
 
-    if (!check_number(second_num_s)) {
-        printf("Incorrect number");
-        exit(1);
-    }
-
-    int first_num = signed_atoi(first_num_s);
-    int second_num = signed_atoi(second_num_s + count_starting_spaces(second_num_s));
+    int first_num = atoi(first_num_s);
+    int second_num = atoi(second_num_s + count_starting_spaces(second_num_s));
     printf("%d", first_num + second_num);
 }
