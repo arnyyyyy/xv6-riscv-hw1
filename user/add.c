33e2f0a28423ee3b1f79cac08b7268d238802f50
arnyyyyy <garrybobcrabsburger@gmail.com>
2024-02-20 22:24:03 +0300

    rewrite using strtok

--- user/add.c
+++ user/add.c
@@ -2,135 +2,61 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int count_spaces(char *str) {
-    int count = 0;
-    while (*str) {
-        if (*str == ' ') {
-            count++;
+#define NULL 0
+#define BUF_SIZE 64
+#define DELM " \n\r"
+
+int all_digits(const char *s) {
+    while (*s != '\0') {
+        if (*s < '0' || '9' < *s) {
+            return 0;
         }
-        str++;
+        s++;
     }
-    return count;
+    return 1;
 }
-
-int count_digits(char *str) {
-    int count = 0;
-    while (*str) {
-        if ((*str) >= '0' && (*str) <= '9') {
-            count++;
-        }
-        str++;
-    }
-    return count;
-}
-
-int count_starting_spaces(char *str) {
-    char *str_cp = str;
-    while (*str_cp == ' ') {
-        str_cp++;
-    }
-
-    return str_cp - str;
-}
-
-int count_ending_spaces(char *str) {
-    char *str_cp = str;
-    char *str_s = str;
-    while (*str_cp != '\0' && *(str_cp + 1) != '\0') {
-        str_cp++;
-    }
-
-    int cnt = 0;
-
-    while (str_cp != str_s && *str_cp == ' ') {
-        cnt++;
-        str_cp--;
-    }
-
-    return cnt;
-}
-
-int count_numbers(char *str) {
-    int seen_digit = 0;
-    int cnt = 0;
-    while (*str != '\0') {
-        if (*str == ' ' && seen_digit) {
-            cnt++;
-        }
-        while (*str == ' ') {
-            str++;
-            seen_digit = 0;
-        }
-
-        while ((*str >= '0' && *str <= '9')) {
-            str++;
-            seen_digit = 1;
-        }
-    }
-
-    if (seen_digit) {
-        cnt++;
-    }
-    return cnt;
-}
-
 
 int main(int argc, char *argv[]) {
-    int BUF_SIZE = 50;
     char buf[BUF_SIZE];
     gets(buf, BUF_SIZE);
 
-    if (strchr(buf, '\n')) {
-        BUF_SIZE = buf - strchr(buf, '\n');
-        *strchr(buf, '\n') = '\0';
-    }
-
-    buf[BUF_SIZE - 1] = '\0';
-    BUF_SIZE = strlen(buf) + 1;
-
-    int spaces = count_spaces(buf);
-    int digits = count_digits(buf);
-    int extra_starting_spaces = count_starting_spaces(buf);
-    int extra_ending_spaces = count_ending_spaces(buf + extra_starting_spaces);
-
-    if (spaces + digits != BUF_SIZE - 1) {
-        printf("Invalid args: unexpected symbol\n");
+    if (strchr(buf, '\n') == NULL) {
+        printf("Invalid args: buffer overflow\n");
         exit(1);
     }
 
-    if (!(digits > 1 && digits < 21)) {  //MAX_INT has 10 digits
-        printf("Invalid args: unexpected number of digits\n");
+    char *lhs_s = strtok(buf, DELM);
+    char *rhs_s = strtok(NULL, DELM);
+    if (lhs_s == NULL) {
+        printf("Invalid number of args: two numbers expected, but none received\n");
         exit(1);
     }
-
-    if (spaces - extra_starting_spaces - extra_ending_spaces == 0) {
-        printf("Invalid number of args: two numbers expected, but less received\n");
+    if (rhs_s == NULL) {
+        printf("Invalid number of args: two numbers expected, but only one received\n");
         exit(1);
     }
-
-    int numbers_counter = count_numbers(buf);
-
-    if (numbers_counter != 2) {
+    if (strtok(NULL, DELM) != NULL) {
         printf("Invalid number of args: two numbers expected, but more received\n");
         exit(1);
     }
-
-    *strchr(buf + extra_starting_spaces, ' ') = '\0';
-
-    char first_num_s[strlen(buf) + 1 - extra_starting_spaces];
-    char second_num_s[BUF_SIZE - strlen(buf) - 1];
-
-    strcpy(first_num_s, buf + extra_starting_spaces);
-    strcpy(second_num_s, buf + strlen(first_num_s) + extra_starting_spaces + 1);
-
-    int first_num = atoi(first_num_s);
-    int second_num = atoi(second_num_s + count_starting_spaces(second_num_s));
-
-    int sum = first_num + second_num;
-    if (sum < 0) {
-        printf("Undefined behaviour while adding two numbers happened\n");
+    if (!all_digits(lhs_s)) {
+        printf("Invalid args: unexpected symbols in the first argument\n");
         exit(1);
     }
-    printf("%d", sum);
+    if (!all_digits(rhs_s)) {
+        printf("Invalid args: unexpected symbols in the second argument\n");
+        exit(1);
+    }
+
+    int lhs = atoi(lhs_s);
+    int rhs = atoi(rhs_s);
+
+    int sum = lhs + rhs;
+    if (sum < 0) {
+        printf("Addition overflow\n");
+        exit(1);
+    }
+
+    printf("%d\n", lhs + rhs);
     exit(0);
 }
